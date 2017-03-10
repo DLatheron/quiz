a9636d7acefe804af03e8d2f7210e6f618bc5db6
DLatheron <davidl@pagn.co.uk>
2017-03-10 17:29:28 +0000

    Initial revision.

---
+++ routes/question.js
@@ -0,0 +1,23 @@
+﻿const express = require('express');
+const router = express.Router();
+const shuffle = require('shuffle-array');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('question', {
+        title: 'Question',
+        number: 1,
+        of: 4,
+        text: 'What is the capital of Sweden?',
+        answers: shuffle([
+            'Berlin',
+            'Paris',
+            'Copenhagen',
+            'Stockholm',
+            'London',
+            'Cardiff'
+        ])
+    });
+});
+
+module.exports = router;

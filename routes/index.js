2d604a78216d8ff2dcb43460746bce60e4357152
dlatheron <David@latheron.plus.com>
2017-05-24 07:44:39 +0100

    Formatting of question time.

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const isAuthenticated = (req, res, next) => {
         return next();
     }
     res.redirect('/');
-}
+};
 
 // GET login page.
 router.get('/', (req, res) => {
--- routes/questionInventory.js
+++ routes/questionInventory.js
@@ -1,9 +1,10 @@
 /* globals module, require */
 'use strict';
 
+const async = require('async');
 const express = require('express');
+const moment = require('moment');
 const router = express.Router();
-
 
 router.get('/', (req, res) => {
     req.db.getQuestions(
@@ -12,11 +13,22 @@ router.get('/', (req, res) => {
             // limit: 10
         }, 
         (error, questions) => {
-            res.render('questionInventory', {
-                title: 'Question Inventory',
-                inventory: questions
-        });
-    });
+            async.map(questions, (question, callback) => {
+                // Remap the author.
+                req.db.getUser({ _id: question.author }, (error, author) => {
+                    question.author = `${author.firstName} ${author.lastName}`;
+                    callback(null, question);
+                });
+            }, (error, updatedQuestions) => {
+                res.render('questionInventory', {
+                    title: 'Question Inventory',
+                    inventory: updatedQuestions,
+                    moment: moment,
+                    timeFormat: 'h:mma, D MMM YYYY z'
+                });
+            });
+        }
+    );
 });
 
 router.post('/questionInventory/', (req, res, next) => {
